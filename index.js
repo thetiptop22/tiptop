c9d12caa7550c365ca97c160a9e98db1006bda1a
thetiptop22 <116219196+thetiptop22@users.noreply.github.com>
2023-02-14 11:40:27 +0100

    update index 2

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log("Hello World from deployhq 2 with Hicham & Abdel & Hakim");
 console.logg("Hello World from deployhq 2 with Hicham & Abdel & Hakim");
-console.logg("Hello Raphael");
+console.logg("Hello Mehdi");
 
